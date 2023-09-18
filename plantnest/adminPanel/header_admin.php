8854352f6acb8759412d729d7898de75b8516966
Rakaan <std@LAB.COM>
2023-09-18 12:15:08 +0500

    logopath

--- plantnest/adminPanel/header_admin.php
+++ plantnest/adminPanel/header_admin.php
@@ -66,7 +66,7 @@ include("php/query.php");
         <!-- Sidebar Start -->
         <div class="sidebar pe-4 pb-3">
             <nav class="navbar bg-light navbar-light">
-                <a href="index.html" class="navbar-brand mx-4 mb-3">
+                <a href="index.php" class="navbar-brand mx-4 mb-3">
                     <div>
                     <h4 class="text-primary"><i class="fa fa-hashtag me-2"></i>PlantNest Panel</h4>
                     </div>
